b2a294f5af6e451d0c098b9f44d95dabab2b67cb
unknown <olena.denysova88@gmail.com>
2022-04-25 16:24:51 +0200

    db connection

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+//Connect Database
+connectDB();
 
-app.get('/', (req, res) => {
-    res.send('API running');
-})
+app.get("/", (req, res) => {
+    res.send("API running");
+});
 
 const PORT = process.env.PORT || 5000;
 
