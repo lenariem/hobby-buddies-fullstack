88303c426db34cf354b4af71e13fe299ca3be091
unknown <olena.denysova88@gmail.com>
2022-04-27 18:32:44 +0200

    jwt token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require('config');
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -31,7 +33,7 @@ router.post(
             let user = await User.findOne({ email });
 
             if (user) {
-                res.status(400).json({
+                return res.status(400).json({
                     errors: [{ msg: "User already exists" }],
                 });
             }
@@ -57,7 +59,22 @@ router.post(
 
             await user.save();
 
-            res.send("User registered");
+            // JWT
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            jwt.sign(
+                payload,
+                config.get("jwtSecret"),
+                { expiresIn: "5 days" },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
         } catch (err) {
             console.error(err.message);
             res.status(500).send("Server error");
