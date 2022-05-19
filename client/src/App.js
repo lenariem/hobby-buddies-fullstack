2ca3a3dc4a126d2c5d0c838fb4f6018c920d3e08
unknown <olena.denysova88@gmail.com>
2022-05-19 12:00:22 +0200

    landing and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/layout/Navbar";
+import { Landing } from "./components/layout/Landing";
 
 function App() {
-  return (
-    <div className="App">
-     <h1>React</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    {/*  <Route path="/about" element={<About />} />
+                    <Route path="/category/:name" element={<Category />}/>
+                    <Route path="/meal/:id" element={<Recipe />}/>
+                    <Route path="*" element={<NotFoundPage />} /> */}
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
