0d548d32cc27fd827fe6c7ff2d3722a4649f890e
unknown <olena.denysova88@gmail.com>
2022-07-07 15:26:49 +0200

    education component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import { getProfileById } from "../../actions/profile";
 import { ProfileTop } from "./ProfileTop";
 import { ProfileAbout } from "./ProfileAbout";
 import { ProfileExperience } from "./ProfileExperience";
+import { ProfileEducation } from "./ProfileEducation";
 
 export const Profile = () => {
     const dispatch = useDispatch();
@@ -49,6 +50,22 @@ export const Profile = () => {
                                 </>
                             ) : (
                                 <h4>No experience credentials</h4>
+                            )}
+                        </div>
+                        
+                        <div className="profile-edu bg-white p-2">
+                            <h2 className="text-primary">Education</h2>
+                            {profile.education.length > 0 ? (
+                                <>
+                                    {profile.education.map(education => (
+                                        <ProfileEducation
+                                            key={education._id}
+                                            education={education}
+                                        />
+                                    ))}
+                                </>
+                            ) : (
+                                <h4>No education credentials</h4>
                             )}
                         </div>
                     </div>
