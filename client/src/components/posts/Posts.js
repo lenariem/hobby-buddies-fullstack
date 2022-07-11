8bde29ba32d62949abd5769dcbde609e6228936e
unknown <olena.denysova88@gmail.com>
2022-07-11 19:00:08 +0200

    post item with bug

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "../../actions/post";
+import { PostItem } from "./PostItem";
 import Spinner from "../layout/Spinner";
 
 export const Posts = () => {
     const dispatch = useDispatch();
-    const loading = useSelector(state => state.profile.loading);
+    const posts = useSelector(state => state.post.posts);
+    console.log("posts" + posts)
+    const loading = useSelector(state => state.post.loading);
 
     useEffect(() => {
         dispatch(getPosts());
@@ -18,7 +21,12 @@ export const Posts = () => {
                 <i className="fas fa-user" /> Welcome to the community
             </p>
             <form />
-            <div className="posts">Posts</div>
+            {loading && <Spinner />}
+            <div className="posts">
+                {posts.map(post => (
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
         </section>
     );
 };
